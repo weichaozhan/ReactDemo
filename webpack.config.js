7346ea25b61282e0eb2246824721be347f4c065a
unknown <zhanweichao@utry.msc>
2018-02-28 17:32:25 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 //__dirname是node.js中的一个全局变量，它指向当前执行脚本所在的目录
 module.exports = {//注意这里是exports不是export
   entry: './src/index.js',//唯一入口文件，就像Java中的main方法
-  output: {//输出目录
-      path: __dirname + '/dist',//打包后的js文件存放的地方
-      filename: 'bundle.js'//打包后的js文件名
-  },
   devtool: 'eval-source-map',
   module: {
     rules: [
@@ -28,5 +24,10 @@ module.exports = {//注意这里是exports不是export
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({template: './src/index.html'})
-  ]
+  ],
+  output: {//输出目录
+    path: __dirname + '/dist', //打包后的js文件存放的地方
+    filename: 'bundle.js', //打包后的js文件名
+    publicPath: '/'
+  }
 };
